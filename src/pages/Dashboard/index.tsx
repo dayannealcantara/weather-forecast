15b01f32ead15813b12c8b31166b2b93b0ad2ec0
Dayanne Alcantara <dayannealcantara14@gmail.com>
2024-07-09 12:03:18 -0300

    feat: starting dashboard page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+
+import { DashboardContainer } from './styles';
+
+export function Dashboard() {
+  
+  return (
+    <DashboardContainer>      
+        
+    </DashboardContainer>
+  );
+}
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+export const DashboardContainer = styled.div `
+display: flex;
+height: 100vh;
+`
+
+
+
+  
+
+
