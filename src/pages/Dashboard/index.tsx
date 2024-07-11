9eeb75849e5171ea0078214f8c72e5a952145f2b
Dayanne Alcantara <dayannealcantara14@gmail.com>
2024-07-10 22:01:46 -0300

    feat: inserting forecast cups and forecast details of the day, next days forecast

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,32 @@
+import { Today } from '../../components/Today';
+import { Details } from '../../components/Details';
+import { NextDays } from '../../components/NextDays';
 
-import { DashboardContainer } from './styles';
+import { Container, WrapperRight } from './styles';
+import { useWeather } from '../../hooks/useWeather';
+import LoadingBackdrop from '../../components/LoadingBackdrop';
 
 export function Dashboard() {
-  
+  const { data, city, isCelsius, setCity, toggleTemperatureUnit, isLoading } =
+    useWeather();
+
+  if (!data || isLoading) {
+    return <LoadingBackdrop />;
+  }
+
   return (
-    <DashboardContainer>      
-        
-    </DashboardContainer>
+    <Container>
+      <Today
+        city={city.name}
+        onSearchValue={setCity}
+        weather={data.today.weather}
+        isCelsius={isCelsius}
+        toggleTemperatureUnit={toggleTemperatureUnit}
+      />
+      <WrapperRight>
+        <Details detailsToday={data.today.details} isCelsius={isCelsius} />
+        <NextDays dataNextDays={data.nextDays} isCelsius={isCelsius} />
+      </WrapperRight>
+    </Container>
   );
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,13 +1,21 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
+export const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  gap: 24px;
+  padding: 24px;
 
-export const DashboardContainer = styled.div `
-display: flex;
-height: 100vh;
-`
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+export const WrapperRight = styled.div`
+  width: 100%;
 
-
-
-  
-
-
+  display: flex;
+  gap: 24px;
+  flex-direction: column;
+  justify-content: space-between;
+`;
