f5031ded7143c903698608caa7b92ced7fbe4a3e
Dayanne Alcantara <dayannealcantara14@gmail.com>
2024-07-10 22:02:12 -0300

    feat: inserting themeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-
 import { Dashboard } from './pages/Dashboard';
 import GlobalStyle from './styles/global';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 import './utils/dayjs.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <Dashboard />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Dashboard />
+    </ThemeProvider>
+  </React.StrictMode>
+);
